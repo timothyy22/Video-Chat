8d1f28b0744cb741be4bc497952d0780e5d0397c
timothy <tsingletary1579@gmail.com>
2024-01-09 22:12:48 -0600

    setup

--- sever.js
+++ sever.js
@@ -10,7 +10,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
-
 app.get('/:room', (req, res) => {
     res.render('room' , { roomId: req.params.room})
 })
