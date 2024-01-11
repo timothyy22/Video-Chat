73377c4a03b6a755259fc3edb1e9f66d62d2d798
timothy <tsingletary1579@gmail.com>
2024-01-11 12:50:53 -0600

    fixed view

--- sever.js
+++ sever.js
@@ -14,4 +14,11 @@ app.get('/:room', (req, res) => {
     res.render('room' , { roomId: req.params.room})
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    })
+})
+
 server.listen(3000)
