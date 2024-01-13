9c840dd79c48712cda3b1944233beaeef0990eeb
timothy <tsingletary1579@gmail.com>
2024-01-12 19:00:51 -0600

    test

--- public/script.js
+++ public/script.js
@@ -3,5 +3,5 @@ const socket = io('/')
 socket.emit('join-room', ROOM_ID, 10)
 
 socket.on('user-connected', userId => {
-    console.log('user connected' + userId)
+    console.log('user connected' + 10)
 })
